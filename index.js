8dd29ddef989983a99aa47386e0eb1268ee6a6d5
AlexBrown4020 <balex3497@gmail.com>
2022-10-03 12:48:34 +0900

    Feat: Require express and dotenv, include port

--- index.js
+++ index.js
@@ -1,5 +1,28 @@
 const cheerio = require('cheerio');
 const axios = require('axios');
+const express =  require('express');
+const dotenv = require('dotenv');
+
+const app = express();
+const PORT = process.env.PORT || 8000
+dotenv.config();
+
+app.use(express.json());
+
+app.use((err,req,res,next) => {
+    const errorStatus = err.status || 500;
+    const errorMessage = err.message || 'Something went wrong, no error message found';
+    return res.status(errorStatus).json({
+        success: false,
+        status: errorStatus,
+        message: errorMessage,
+        stack: err.stack,
+    });
+})
+
+app.listen(PORT, () => {
+    console.log(`Connected to backend on port ${PORT}`);
+});
 
 const searchString = 'cosplay convention';
 const encodedString = encodeURI(searchString);
