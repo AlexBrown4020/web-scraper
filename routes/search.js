cc6514cf2ec898286860642f9d3d190421475939
AlexBrown4020 <balex3497@gmail.com>
2022-10-04 09:06:18 +0900

    Feat: Make getConventionInfo available to global scope

--- routes/search.js
+++ routes/search.js
@@ -12,43 +12,42 @@ const AXIOS_OPTIONS = {
     },
 };
 
+function getConventionInfo() {
+    return axios.get(`${domain}/search?q=${encodedString}&hl=en@gl=us`, AXIOS_OPTIONS)
+    .then(function ({ data }) {
+        let $ = cheerio.load(data);
+
+        const links = [];
+        const titles = [];
+        const snippets = [];
+
+        $('.yuRUbf > a').each((i, el) => {
+            links[i] = $(el).attr('href');
+        });
+        $('.yuRUbf > a > h3').each((i, el) => {
+            titles[i] = $(el).text();
+        });
+        $('.lyLwlc').each((i, el) => {
+            snippets[i] = $(el).text().trim();
+        });
+
+        const result = [];
+        for (let i = 0; i < links.length; i++) {
+            result[i] = {
+                link: links[i],
+                title: titles[i],
+                snippet: snippets[i],
+            };
+        };
+        return result;
+    }); 
+}
 
 const router = express.Router();
 
 router.get('/', async (req, res, next) => {
     try {
-        function getConventionInfo() {
-            return axios.get(`${domain}/search?q=${encodedString}&hl=en@gl=us`, AXIOS_OPTIONS)
-            .then(function ({ data }) {
-                let $ = cheerio.load(data);
-        
-                const links = [];
-                const titles = [];
-                const snippets = [];
-        
-                $('.yuRUbf > a').each((i, el) => {
-                    links[i] = $(el).attr('href');
-                });
-                $('.yuRUbf > a > h3').each((i, el) => {
-                    titles[i] = $(el).text();
-                });
-                $('.lyLwlc').each((i, el) => {
-                    snippets[i] = $(el).text().trim();
-                });
-        
-                const result = [];
-                for (let i = 0; i < links.length; i++) {
-                    result[i] = {
-                        link: links[i],
-                        title: titles[i],
-                        snippet: snippets[i],
-                    };
-                };
-                return result;
-            }); 
-        }
         const output = await getConventionInfo();
-        console.log(output)
         res.status(200).json(output)
     } catch(err) {
         next(err);
