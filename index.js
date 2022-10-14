ffc5bb1926e28ce4fea324abf24d82120ff6cfc7
AlexBrown4020 <balex3497@gmail.com>
2022-10-14 15:00:30 +0900

    Fix: Change build script for firebase

--- index.js
+++ index.js
@@ -5,6 +5,10 @@ import cors from 'cors';
 import Convention from './routes/Convention.js';
 import Character from './routes/Characters.js'
 
+const firebaseApp = initializeApp({
+
+})
+
 const app = express();
 const PORT = process.env.PORT || 8000
 dotenv.config();
